d5de5abb7def77da90459d31ef73597def45d9e8
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-23 13:10:17 +0100

    build out the navbar and apply styles

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 const NavBar = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <View style={styles.bar}></View>
+      <View style={styles.bar}></View>
+      <View style={styles.bar}></View>
+      <View style={styles.bar}></View>
+      <View style={styles.bar}></View>
     </View>
   );
 };
 
 export default NavBar;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 16,
+    paddingBottom: 16,
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row"
+  },
+  bar: {
+    width: "18%",
+    height: 4,
+    borderRadius: 5,
+    backgroundColor: "#274fed"
+  }
+});

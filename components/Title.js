08007f641b4ae6ea4a017676ad3e3b7f10dfee95
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-23 13:11:24 +0100

    apply styles to the headline

--- components/Title.js
+++ components/Title.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 const Title = () => {
   return (
-    <View>
-      <Text>Complete profile</Text>
-      <Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Complete profile</Text>
+      <Text style={styles.subtitle}>
         Tell us even more about yourself and ensure that all details provided
         herein are valid and up to date.
       </Text>
@@ -14,3 +14,21 @@ const Title = () => {
 };
 
 export default Title;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 16,
+    paddingBottom: 16
+  },
+  heading: {
+    color: "#274fed",
+    fontSize: 32,
+    fontWeight: "bold",
+    paddingTop: 16,
+    paddingBottom: 8
+  },
+  subtitle: {
+    paddingTop: 8,
+    paddingBottom: 16
+  }
+});

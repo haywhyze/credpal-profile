24b50e09f28486c45fc887737899f372adfe7553
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-23 13:09:48 +0100

    apply styles to topBar

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 const TopBar = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <FontAwesomeIcon icon="angle-left" />
+      <Text style={styles.text}>Step 1 of 5</Text>
     </View>
   );
 };
 
 export default TopBar;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  text: {
+    color: "#555"
+  }
+});

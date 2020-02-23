11a5d27fb87ebb93f9480c2d6ca2a823d987ae34
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-23 13:07:28 +0100

    add icons library from fontawesome and adjusted layout

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCalendar,
+  faVenusMars,
+  faHome,
+  faGraduationCap,
+  faSuitcase,
+  faFlag,
+  faLifeRing,
+  faUser,
+  faLink,
+  faPhone,
+  faAngleLeft,
+  faLock
+} from "@fortawesome/free-solid-svg-icons";
 
-import Header  from './components/Header';
-import Main  from './components/Main';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
       <Main />
+      <Footer />
     </View>
   );
 }
 
+library.add(
+  faCalendar,
+  faPhone,
+  faAngleLeft,
+  faLifeRing,
+  faUser,
+  faLink,
+  faFlag,
+  faVenusMars,
+  faGraduationCap,
+  faHome,
+  faSuitcase,
+  faLock,
+);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 32,
+    minHeight: "100vh"
+  }
 });
